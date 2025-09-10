193d956fbb4831e0c1d7c608346ba8d5df9bbb63
Bowen1123 <131422130+Bowen1123@users.noreply.github.com>
2025-09-10 11:47:02 -0500

    Setup

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollBot/Teleop.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.teamcode.TrollBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class Teleop extends OpMode {
+
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
