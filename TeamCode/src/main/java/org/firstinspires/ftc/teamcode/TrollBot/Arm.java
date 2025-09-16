6f9580b2b239545d049d430f2e691d9174d2a0e1
Bowen1123 <131422130+Bowen1123@users.noreply.github.com>
2025-09-15 19:57:25 -0500

    Concepts from Bowen

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollBot/Arm.java
@@ -0,0 +1,24 @@
+package org.firstinspires.ftc.teamcode.TrollBot;
+
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+@Config
+@TeleOp
+public class Arm{
+    private DcMotor arm;
+
+ // will finish later
+    private static double p = 0, i = 0, d = 0, f = 0;
+    public Arm() {
+
+
+
+        arm = hardwareMap.get(DcMotor.class, "arm");
+
+
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollBot/Teleop_Mutithread.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode.TrollBot;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Teleop_Mutithread extends LinearOpMode {
+
+
+    private class TempThread implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+    }
+}
